4f45ae7476d9bde453be17c1d00df29cc4479552
Soham Chowdhury <redacted.coder@gmail.com>
2013-05-04 14:00:11 +0530

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Whythequiz::Application.config.secret_token = '4a4a05305a3b00b62a0d45c2a118bd767533a74dac67ee8c8a20bbc75882a272a2050fe1a6002214feb6b8aebc5b9eb316fdfee9f668b588aab246e5f7866f6e'
